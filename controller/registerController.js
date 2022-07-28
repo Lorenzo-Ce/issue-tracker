803df65c5d1c95a66f5cc7ea4d853ba7fb36715d
lorenzo <lorenzo.cester.lc@gmail.com>
2022-07-28 17:36:00 +0200

    fixed  tests + /register path

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,5 +1,38 @@
-const registerNewUser = (req, res, err) => {
-    res.sendStatus(201)    
+const User = require('../model/User')
+const bcrypt = require('bcrypt')
+
+const registerNewUser = async (req, res, err) => {
+    const {email, username, password} = req.body
+    if(!email || !username || !password){
+        return res.status(400).send({"Error" : `Missing one or more of the required field: Email, password, username`})
+    }
+    const doubleUser = await User.findOne({
+        $or: [
+            { 'username': username }, 
+            { 'email': email }
+        ]
+    }).exec()
+    if(doubleUser){
+        return res.status(409)
+                .send({'Error' : `${doubleUser.email === email ? 'Email already present in DB' : 'Username already taken'}`})
+    }
+     try{
+            const hashedPsw = await bcrypt.hash(password, 11)
+            await User.create ({
+                email,
+                username,
+                password: hashedPsw,
+                isAuthenticated: false,
+            })
+            return res.status(201)
+                    .send({
+                        'message': `User created`,
+                        'username': `${username}`                                    
+                    })
+        }
+        catch(err){
+            res.status(500).send({'Error' : `${err.message}`})
+        }
 }
 
 module.exports = {registerNewUser}
--- test/server.test.js
+++ test/server.test.js
@@ -1,21 +1,39 @@
 const app = require('../app')
-var supertest = require("supertest")(app);
+const request = require('supertest')(app);
 
-describe("API test root", function(){
-  it("GET '/' should return 200", function(done) {
-    supertest
-      .get("/")
-      .expect(200)
-      .end(done)
-
+describe('API test root', () => {
+  test('GET / should return 200', async () => {
+    const response = await request.get('/')
+      expect(response.statusCode).toBe(200)
     })
 })
 
-describe("API /register", function(){
-  it("POST '/register' should return 201", function(done) {
-    supertest
-      .post("/register")
-      .expect(201) 
-      .end(done)
+describe('API /register', () => {
+  test('POST /register should return 201', async () => {
+    const response = await request.post('/register')
+      .set('Accept', 'application/json')
+      .send(({
+        username : 'test',
+        password: 'test1234',
+        email : 'test@example.com'
+      }))
+      expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
+      expect(response.statusCode).toBe(201)
+      expect(response.body.username).toBe('test')
     })
+
+  describe('API /register duplicate mail', () => {
+    test('POST /register should return 409', async () =>{
+      const response = await request.post('/register')
+      .set('Accept', 'application/json')
+      .send(({
+        username : 'test11',
+        password: 'test1234',
+        email : 'test@example.com'
+      }))
+      expect(response.headers['content-type']).toBe('application/json; charset=utf-8')
+      expect(response.statusCode).toBe(409)
+      expect(response.body).toHaveProperty('Error', 'Email already present in DB')
+    })
+  })
 })
