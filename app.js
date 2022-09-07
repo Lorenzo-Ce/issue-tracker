4073951a6764cb60f7acca96c6c76f08d2f5d614
lorenzo <lorenzo.cester.lc@gmail.com>
2022-09-07 07:10:16 +0200

    add sanitaze package

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize');
 const { logger } = require('./middleware/logHandler')
 const { errorHandler } = require('./middleware/errorHandler')
 
@@ -15,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-
+app.use(mongoSanitize())
 
 app.use('/register', require('./routes/register'))
 app.use('/login', require('./routes/login'))
