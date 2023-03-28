37a8b919db0d3ba689fedd89e23984ad0fa56dd4
lorenzo <lorenzo.cester.lc@gmail.com>
2023-03-28 10:05:17 +0200

    update allowedDomains based on deploy on render

--- server/config/allowedDomains.js
+++ server/config/allowedDomains.js
@@ -1,8 +1,9 @@
 const allowedDomains = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'http://localhost:3500',
-     'https://www.thunderclient.com'
+    // 'http://localhost:3500',
+    // 'https://www.thunderclient.com',
+    'https://issue-tracker-client.onrender.com'
 ]
 
 module.exports = allowedDomains
